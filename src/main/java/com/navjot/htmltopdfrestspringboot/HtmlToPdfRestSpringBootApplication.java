cc070987cac1bfd8e6e54815816920dfb69d5e84
Navjot Singh <navjotsingh@Navjots-MacBook-Air.local>
2023-01-31 17:50:35 -0500

    project creation

---
+++ src/main/java/com/navjot/htmltopdfrestspringboot/HtmlToPdfRestSpringBootApplication.java
@@ -0,0 +1,13 @@
+package com.navjot.htmltopdfrestspringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HtmlToPdfRestSpringBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HtmlToPdfRestSpringBootApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/navjot/htmltopdfrestspringboot/HtmlToPdfRestSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.navjot.htmltopdfrestspringboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HtmlToPdfRestSpringBootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
